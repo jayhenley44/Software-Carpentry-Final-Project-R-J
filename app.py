bc0a8596c3e37f4a7d84c552afc6eb0399b7543f
jayhenley44 <129785491+jayhenley44@users.noreply.github.com>
2023-05-07 15:14:18 -0400

    changes to app.py and base and index to display  the added bootstrap stuff

--- app.py
+++ app.py
@@ -5,7 +5,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', year=datetime)
+    return render_template('index.html', year=datetime.now().year)
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html', year=datetime.now().year)
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', year=datetime.now().year)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', year=datetime.now().year)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', year=datetime.now().year)
 
 if __name__ == '__main__':
     app.run(debug=True)
